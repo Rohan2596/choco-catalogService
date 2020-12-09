04c27ac9ca806ecac2318d1e49886cf8344e9546
Rohan2596 <rohankadam662@gmail.com>
2020-12-09 14:15:35 +0530

    ADDED:Express validators and create catalog controller logic.

--- controller/SupplierController.js
+++ controller/SupplierController.js
@@ -1,7 +1,30 @@
 class SupplierController{
  
-    createCatalog=(req,res)=>{
-        console.log("create Catalog for supplier");
+    createCatalog=(req,res,next)=>{
+        try {
+            let response={}
+            req.checkBody('name','Please Enter Valid Catalog name!').notEmpty().isAlpha().isLength({min:'3',max:'10'});
+            req.checkBody('category','Please Enter Valid Catalog category!').notEmpty().isAlpha().isLength({min:'3',max:'10'});
+            let validationErrors=req.validationErrors();
+            if(validationErrors){
+                response.success=false;
+                response.message="Invalid Credentials for catalog!";
+                response.data=validationErrors[0].msg;
+                return res.status(500).send(response);
+            }else{
+                const catalog={
+                    'name':req.body.name,
+                    'category':req.body.category,
+                    'items':[]
+                }
+                response.success=true;
+                response.message="Catalog Created SuccessFully!";
+                response.data=catalog;
+               return res.status(200).send(response);
+            }
+        } catch (error) {
+            next(error)
+        }
     };
     addItemtoCatalog=(req,res)=>{
         console.log("Adding Item to catalog");
--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express=require('express');
 const app=express();
 const route=require('./routes/route');
+const bodyParser=require('body-parser');
+const createError=require('http-errors')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+
+
+
+//Express Validator
+const expressValidator=require('express-validator');
+app.use(expressValidator());
+
 
 app.use("/choco/customer/catalog",route);
 
 
 
 
+app.use((req,res,next)=>{
+    next(createError(404));
+})
 
+app.use((error,req,res,next)=>{
+    let response={
+        success:false,
+        status:500,
+        message:error.message
+    };
+    res.json(response);
+})
 
 
 
