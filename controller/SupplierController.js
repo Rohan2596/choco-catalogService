6ce0563b4c422f09d9156d4113d009d6d864338b
Rohan2596 <rohankadam662@gmail.com>
2020-12-09 16:04:23 +0530

    ADDED:Add item and edit item controller logic

--- controller/SupplierController.js
+++ controller/SupplierController.js
@@ -1,49 +1,86 @@
-class SupplierController{
- 
-    createCatalog=(req,res,next)=>{
+class SupplierController {
+
+    createCatalog = (req, res, next) => {
         try {
-            let response={}
-            req.checkBody('name','Please Enter Valid Catalog name!').notEmpty().isAlpha().isLength({min:'3',max:'10'});
-            req.checkBody('category','Please Enter Valid Catalog category!').notEmpty().isAlpha().isLength({min:'3',max:'10'});
-            let validationErrors=req.validationErrors();
-            if(validationErrors){
-                response.success=false;
-                response.message="Invalid Credentials for catalog!";
-                response.data=validationErrors[0].msg;
+            let response = {}
+            let token = req.params.token
+
+            req.checkBody('name', 'Please Enter Valid Catalog name!').notEmpty().isAlpha().isLength({ min: '3', max: '10' });
+            req.checkBody('category', 'Please Enter Valid Catalog category!').notEmpty().isAlpha().isLength({ min: '3', max: '10' });
+            let validationErrors = req.validationErrors();
+            if (validationErrors) {
+                response.success = false;
+                response.message = "Invalid Credentials for catalog!";
+                response.data = validationErrors[0].msg;
                 return res.status(500).send(response);
-            }else{
-                const catalog={
-                    'name':req.body.name,
-                    'category':req.body.category,
-                    'items':[]
+            } else {
+                const catalog = {
+                    'name': req.body.name,
+                    'category': req.body.category,
+                    'items': []
                 }
-                response.success=true;
-                response.message="Catalog Created SuccessFully!";
-                response.data=catalog;
-               return res.status(200).send(response);
+                response.success = true;
+                response.message = "Catalog Created SuccessFully!";
+                response.data = catalog;
+                return res.status(200).send(response);
             }
         } catch (error) {
             next(error)
         }
     };
-    addItemtoCatalog=(req,res)=>{
-        console.log("Adding Item to catalog");
+    addItemtoCatalog = (req, res, next) => {
+
+        try {
+            let response = {}
+            let token = req.params.token
+            let catalogId = req.params.catalogId
+            console.log(token + " <===> " + catalogId);
+            req.checkBody('name', 'Please Enter Valid Item name!').notEmpty().isAlpha().isLength({ min: '3', max: '10' });
+            req.checkBody('category', 'Please Enter Valid Item category!').notEmpty().isAlpha().isLength({ min: '3', max: '10' });
+            req.checkBody('quantity', 'Please Enter Valid Item quantity!').notEmpty().isNumeric().isLength({ min: '1', max: '4' });
+            req.checkBody('price', 'Please Enter Valid price per case !').notEmpty().isNumeric().isLength({ min: '1', max: '4' });
+            req.checkBody('quantityType', 'Please Enter Valid quantity type ie case or box').notEmpty().isAlpha();
+
+            let validationErrors = req.validationErrors();
+            if (validationErrors) {
+                response.success = false;
+                response.message = "Invalid Credentials for item!";
+                response.data = validationErrors[0].msg;
+                return res.status(500).send(response);
+            } else {
+                const item = {
+                    'name': req.body.name,
+                    'category': req.body.category,
+                    'quantity': req.body.quantity,
+                    'price': req.body.price,
+                    'quantityType': req.body.quantityType
+                }
+                response.success = true;
+                response.message = "Item Added SuccessFully!";
+                response.data = item;
+                return res.status(200).send(response);
+            }
+
+        } catch (error) {
+            next(error)
+        }
+
     };
-    editItemofCatalog=(req,res)=>{
+    editItemofCatalog = (req, res) => {
         console.log("Editing Item of Catalog");
     };
-    getAllItemsofCatalog=(req,res)=>{
+    getAllItemsofCatalog = (req, res) => {
         console.log("Getting All Item of Catalog");
     };
-    deleteItemofCatalog=(req,res)=>{
+    deleteItemofCatalog = (req, res) => {
         console.log("Deleting Item from  Catalog permanently.");
     };
-    removeItemFromCatalog=(req,res)=>{
+    removeItemFromCatalog = (req, res) => {
         console.log("Removing Item from Catalog");
     }
-    
+
 
 
 }
 
-module.exports=new SupplierController();
+module.exports = new SupplierController();
